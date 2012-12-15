5de959d17b58a47bc231df9ed4df086dda3d772e
Pedro Rodrigues <medecau@gmail.com>
2012-12-15 00:49:22 +0000

    output file description

--- censos_stretcher.py
+++ censos_stretcher.py
@@ -1,5 +1,6 @@
 import couchdb
 import csv
+import os
 
 #couch = couchdb.Server()
 #db = couch['censos2011']
@@ -8,8 +9,23 @@ def loadfields(row):
     # APPLY A FEW TRANSFORMATIONS TO THE FIELDS
     return [str(cell).strip().decode('latin-1') for cell in row]
 
-with open('Q1.01..csv', 'r') as file:
-    for row in csv.reader(file):
-        print loadfields(row)
+def getdescription(file):
+    with open(file, 'r') as f:
+        r = csv.reader(f)
+        row = loadfields(r.next())
+        return row[0]
+
+
+#with open('Q1.01..csv', 'r') as file:
+#    for row in csv.reader(file):
+#        print loadfields(row)
+
+
+
+
+for p in os.listdir(os.curdir):
+    if p.endswith('csv'):
+        print getdescription(p)
+
 
 
