636f22f460ec17e126e8d274d9300026746f6f48
Pedro Rodrigues <medecau@gmail.com>
2013-05-16 13:07:14 +0100

    pep8

--- fixFilenames.py
+++ fixFilenames.py
@@ -3,19 +3,18 @@ import os.path as path
 import re
 
 
-
 for item in os.listdir("data"):
     print item
 
     # fix duplicate dots
     filename = "data/" + item
-    new_filename = re.sub("\.+",".", filename)
+    new_filename = re.sub("\.+", ".", filename)
     os.rename(filename, new_filename)
     filename = new_filename
 
     # fix missing dots
     if filename[7] != ".":
-        new_filename = "%s.%s" % (filename[:7],filename[7:])
+        new_filename = "%s.%s" % (filename[:7], filename[7:])
         os.rename(filename, new_filename)
     else:
         pass
